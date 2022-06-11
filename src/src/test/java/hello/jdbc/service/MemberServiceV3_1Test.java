1eec483ffb25d79d45a486ec5503b22042da1315
Jo_Minjun <alswns9288@gmail.com>
2022-06-12 05:29:41 +0900

    [ADD] 트랜잭션 문제 해결 - 트랜잭션 매니저

--- src/src/test/java/hello/jdbc/service/MemberServiceV3_1Test.java
+++ src/src/test/java/hello/jdbc/service/MemberServiceV3_1Test.java
@@ -1,12 +1,14 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV2;
+import hello.jdbc.repository.MemberRepositoryV3;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import java.sql.SQLException;
 
@@ -14,20 +16,24 @@ import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class MemberServiceV2Test {
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+class MemberServiceV3_1Test {
 
     static final String MEMBER_A = "memberA";
     static final String MEMBER_B = "memberB";
     static final String MEMBER_EX = "ex";
 
-    MemberRepositoryV2 memberRepository;
-    MemberServiceV2 memberService;
+    MemberRepositoryV3 memberRepository;
+    MemberServiceV3_1 memberService;
 
     @BeforeEach
     void before() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        memberRepository = new MemberRepositoryV2(dataSource);
-        memberService = new MemberServiceV2(dataSource, memberRepository);
+        memberRepository = new MemberRepositoryV3(dataSource);
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+        memberService = new MemberServiceV3_1(transactionManager, memberRepository);
     }
 
     @AfterEach
